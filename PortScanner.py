6aaf6c679a20be970c2c91294f7b24e48c3055f4
Elliot Alderson <98410648+ElliotTerminal@users.noreply.github.com>
2022-08-21 16:50:47 +0530

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+import time
+import threading
+
+usage = "Usage -> python3 portscanner.py (TARGET) (START_PORT) (END_PORT)"
+
+print(" ______ ___________ _____   _____ _____   ___   _   _  _   _  ___________  ")
+print(" | ___ \  _  | ___ \_   _| /  ___/  __ \ / _ \ | \ | || \ | ||  ___| ___ \ ")
+print(" | |_/ / | | | |_/ / | |   \ `--.| /  \// /_\ \|  \| ||  \| || |__ | |_/ / ")
+print(" |  __/| | | |    /  | |    `--. \ |    |  _  || . ` || . ` ||  __||    /  ")
+print(" | |   \ \_/ / |\ \  | |   /\__/ / \__/\| | | || |\  || |\  || |___| |\ \  ")
+print(" \_|    \___/\_| \_| \_/   \____/ \____/\_| |_/\_| \_/\_| \_/\____/\_| \_| ")
+
+start_time = time.time()
+
+if(len(sys.argv) != 4):
+    print(usage)
+    sys.exit()
+
+try:
+    target = socket.gethostbyname(sys.argv[1])
+except socket.gaierror:
+    print("Name Rsolution Error")
+    sys.exit()
+
+start_port = int(sys.argv[2])
+end_port = int(sys.argv[3])
+
+print("Scanning Target ....",target)
+
+def scan_port(port):
+    #print("Scanning Port: ", port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    conn = s.connect_ex((target, port))
+    if(not conn):
+        print("Port {} is OPEN".format(port))
+    s.close()
+
+for port in range(start_port, end_port+1):
+    
+    thread = threading.Thread(target = scan_port, args = (port,))
+    thread.start()
+
+end_time = time.time()
+print("Time Elapsed: ", end_time - start_time, "s")
